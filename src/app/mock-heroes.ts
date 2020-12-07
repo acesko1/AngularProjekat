0070e8c833b35e443ba7c10e5250c7b673a38d93
acesko1 <acesko1@etf.unsa.ba>
2020-12-07 21:04:15 +0100

    Komit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,20 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: "Projekat 1 - zadatak 3" },
+  { id: 12, name: "Projekat 2 - zadatak 4" },
+  { id: 13, name: "Projekat 1 - zadatak 5" },
+  { id: 14, name: "Projekat 4 - zadaktak 4" },
+  { id: 15, name: "Projekat 5 - zadatak 8" },
+  { id: 16, name: "Projekat 8 - zadatak 9" },
+  { id: 17, name: "Projekat 1 - zadatak 2" },
+  { id: 18, name: "Projekat 1 - zadatak 3" },
+  { id: 19, name: "Projekat 5 - zadatak 6" },
+  { id: 20, name: "Projekat 6 - zadatak 9" }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
